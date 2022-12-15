2a0db7dc418afbe5742488d0ae1bdb4b23d2d380
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2022-12-15 23:25:01 +0900

    조금

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ def main() :
     df= pd.read_csv('data/editdrama.csv',index_col =0)
     df_genre= pd.read_csv('data/genre_list.csv',index_col = 'genre')
 
+    df_main = df.loc[:,['drama_name','Episodes','platforms','actors','imdb_description','Genres']]
+
     selected_list = st.multiselect('장르를 선택하세요',df_genre )
-    st.dataframe(df[df['Genres'].str.contains('historical',na=False)])
+    st.dataframe(df[df['Genres'].str.contains(str(selected_list),na=False)])
+    st.dataframe(df_main[df['Genres'].str.contains(str(selected_list),na=False)])
 
 
 if __name__ == '__main__':
