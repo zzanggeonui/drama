60b2a6746edb622e5b82dd98c7eea8e583ce717d
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2022-12-16 17:08:29 +0900

    ok

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import pandas as pd
 
 def main() :
     df= pd.read_csv('data/editdrama.csv',index_col =0)
-    df_genre= pd.read_csv('data/genre_list.csv',index_col = 'genre')
+    df_genre= pd.read_csv('data/gentttttt.csv',index_col = 'genre')
 
-    df_main = df.loc[:,['drama_name','Episodes','platforms','actors','imdb_description','Genres']]
-
+    df_main = df.loc[:,['drama_name','actors','imdb_description','Genres']]
     selected_list = st.multiselect('장르를 선택하세요',df_genre )
-    st.dataframe(df[df['Genres'].str.contains(str(selected_list),na=False)])
-    st.dataframe(df_main[df['Genres'].str.contains(str(selected_list),na=False)])
+    
+    if len(selected_list) != 0 :
 
+        st.dataframe(df[df['Genres'].str.contains(str('war'),na=False)])
+        st.dataframe(df_main[df['Genres'].str.contains(str('war'),na=False)])
+        st.(selected_list)
 
 if __name__ == '__main__':
     main()
