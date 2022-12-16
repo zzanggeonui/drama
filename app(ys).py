25fac54c703c7d5bec7634e75c4917d0831128a5
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2022-12-16 17:09:34 +0900

    ok

---
+++ app(ys).py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+def main() :
+    df= pd.read_csv('data/editdrama.csv',index_col =0)
+    df_genre= pd.read_csv('data/genre_list.csv',index_col = 'genre')
+
+    df_main = df.loc[:,['drama_name','Episodes','platforms','actors','imdb_description','Genres']]
+
+    selected_list = st.multiselect('장르를 선택하세요',df_genre )
+    st.dataframe(df[df['Genres'].str.contains(str(selected_list),na=False)])
+    st.dataframe(df_main[df['Genres'].str.contains(str(selected_list),na=True)])
+
+
+if __name__ == '__main__':
+    main()
