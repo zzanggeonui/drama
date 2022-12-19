8c7fcb41dc0ddc573b1b8d987a9f1d95ff56135b
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2022-12-19 18:08:29 +0900

    himdeum

--- app(ys).py
+++ app(ys).py
@@ -1,15 +1,22 @@
 import streamlit as st
 import pandas as pd
+from app_home import run_home_app
+from app_2 import run_2_app
+
 
 def main() :
-    df= pd.read_csv('data/editdrama.csv',index_col =0)
-    df_genre= pd.read_csv('data/genre_list.csv',index_col = 'genre')
+    st.title('드라마')
+    menu = ['홈','추천','차트']
+    choice = st.sidebar.selectbox('메뉴',menu)
 
-    df_main = df.loc[:,['drama_name','Episodes','platforms','actors','imdb_description','Genres']]
+    if choice == '홈' :
+        run_home_app()
 
-    selected_list = st.multiselect('장르를 선택하세요',df_genre )
-    st.dataframe(df[df['Genres'].str.contains(str(selected_list),na=False)])
-    st.dataframe(df_main[df['Genres'].str.contains(str(selected_list),na=True)])
+    elif choice == '추천' :
+        run_2_app()
+
+    elif choice == '차트':
+        pass
 
 
 if __name__ == '__main__':
---
+++ app_2.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+def run_2_app() :
+    df= pd.read_csv('data/realeditdrama.csv',index_col =0)
+    df_genre= pd.read_csv('data/gentttttt.csv',index_col = 'genre')
+    df_main = df.loc[:,['drama_name','Episodes','platforms']]
+    df_actor = pd.read_csv('data/actor_list.csv',index_col = 'actor')
+    df_tag = pd.read_csv('data/tag_list.csv')
+
+    select = st.selectbox('장르선택',df_genre)
+
+    filter = df_main[df['Genres'].str.contains(str(select),na=False)]
+    
+    select2 =  st.selectbox('배우선택',df_actor)
+    select3 =  st.selectbox('tag선택',df_tag)
+    st.dataframe(filter[df['actors'].str.contains(str(select2),na=False)],)
+    
+
+    st.dataframe(df_main[df['actors'].str.contains(str(select2),na=False)],)
