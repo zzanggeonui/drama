19ed8e08ffb282144bde6d94855b6e8a3183acf7
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2022-12-21 01:09:00 +0900

    열심히

--- app_ml.py
+++ app_ml.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import altair as alt
 
-
-def run_() :
+def run_ml_app() :
     po_sc = pd.read_csv('data/popula_score.csv')    
+    genre_list = pd.read_csv('data/genre_sum.csv').head(10)
+    fig = px.pie(genre_list, 'genre','sum',title='장르별 차트')
+    st.plotly_chart(fig)
     
-
+    
 
 
 
