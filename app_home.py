1070c309e8966e87f07d8d7dad58f4b3e608f507
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2022-12-23 17:59:40 +0900

    바꾸기

--- app_home.py
+++ app_home.py
@@ -7,8 +7,7 @@ translator = Translator()
 from PIL import Image
 
 def run_home_app() :
-    st.title('🔥🔥K-DRAMA의 모든것🔥🔥')
-    url = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/korean-drama-series-1654114746.png?crop=0.502xw:1.00xh;0.250xw,0&resize=640:*'
+    st.title('📺 K-Drama 데이터센터')
     url1 = 'https://kpophighindia.com/wp-content/uploads/2020/01/drama-cover.png'
 
     st.image(url1)
@@ -19,8 +18,8 @@ def run_home_app() :
         
         df_main = df.loc[:,['drama_name','Genres','Tags','Content Rating','Duration','scored by','Watchers','score','Popularity','Episodes','actors','platforms']]
         st.dataframe(df_main)
-        st.text('Genres = 장르,  Tags= 태그,  Content Rating=시청등급,  Duration = 러닝타임,  scored by = 리뷰참여자수  \nscore = 평점,   Watchers = 사이트 조회수,   score = 점수  Popularity = 인기도  Episodes = 에피소드 숫자  \nactors = 배우이름,  platforms = 현재 드라마를 볼 수있는 플랫폼')
+        st.text('●Genres = 장르  \n●Tags= 태그 \n●Content Rating=시청등급  \n●Duration = 러닝타임 \n●scored by = 리뷰참여자수  \n●score = 평점  \n●Watchers = 사이트 조회수   \n●score = 점수 \n●Popularity = 인기도  \n●Episodes = 에피소드 숫자  \n●actors = 배우이름  \n●platforms = 현재 드라마를 볼 수있는 플랫폼')
 
     st.info('드라마 및 영상물 리뷰 전문 사이트인 IMDb와 MyDramaList의 한국 드라마 데이터를 기반으로 한국드라마 흥행요인 분석 및 검색 기능을 구현한 웹 대시보드 입니다. ')
-    
+    st.write('데이터 제공 :https://www.kaggle.com/datasets/iphigeniebera/korean-drama-list-about-740-unique-dramas')
     
--- app_serch.py
+++ app_serch.py
@@ -4,44 +4,56 @@ import plotly.express as px
 import altair as alt
 
 
-def run_sc_app() :
-    df = pd.read_csv('data/realeditdrama.csv', thousands = ',')
-    df_main = df.loc[:,['drama_name','Content Rating','score','Popularity','Episodes','platforms']]
+def  :
+    df = pd.read_csv('data/realeditdrama.csv', thousands = ',',index_col='Unnamed: 0')
+    df_main = df.loc[:,['drama_name','Content Rating','scored by','score','Popularity','Episodes','Genres','Tags','platforms']]
     
+
     st.title('항목별 상세 검색 페이지 입니다')
-    select_rank = st.selectbox(' ', ['전체','장르별','태그별','배우별'])
-    status = st.radio('드라마랭킹 top100',['인기순','평점순','화제순'])
+    select_rank = st.selectbox(' ', ['전체','장르','태그','배우','평점'])
+    
+
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+    platforms = df['platforms']
+    platforms_list = []
+    for x in platforms :
+        x = x.split(',')
+        for x2 in x :
+            x2 = x2.strip(" ")
+            x2 = x2.strip("''")
+            platforms_list.append(x2)
+    plat = pd.DataFrame(pd.DataFrame(data =platforms_list).value_counts()).rename(columns={0:'sum'}).reset_index().rename(columns={0:'platforms'}).drop(1,axis=0)
+
 
     if select_rank =='전체' :
-        
+        status = st.radio('',['인기순','평점순','화제순'])
+        plat_select = st.selectbox('플랫폼 선택',plat)
+
         if status == '인기순' :
-            st.dataframe(df.loc[:,['drama_name','Popularity']].sort_values('Popularity').head(100))
+            st.dataframe(df_main.sort_values('Popularity').head(100))
                 
         elif status == '평점순' :
-            st.dataframe(df.loc[:,['drama_name','score']].sort_values('score',ascending=False).head(100) )
+            st.dataframe( df_main[df_main['scored by'] >10].sort_values('score',ascending=False).head(100)    )
         elif status == '화제순' :
-            st.dataframe(df.loc[:,['drama_name','scored by']].sort_values('scored by',ascending=False ).head(100))
-        elif status == '조회순' :
-            st.dataframe(df.loc[:,['drama_name','Watchers']].sort_values('Watchers',ascending=False).head(100) )
-
+            st.dataframe(df_main.sort_values('scored by',ascending=False ).head(100))
+        
 
     if select_rank =='장르별' :
-        genre_scpore = pd.read_csv('data/genre_scpore.csv',index_col='genre')
-        genre_sum = pd.read_csv('data/gnre_sum.csv')
+        genre_scpore = pd.read_csv('data/genre_scporesum.csv',index_col='genre')
+        status = st.radio('',['인기순','평점순','화제순'])
 
         if status == '인기순':
            
             st.subheader('시청자로부터 사랑을 받은 작품이 많은 장르순입니다')
-            st.dataframe(genre_scpore.loc[:,['popul']].seort_values('popul'))
-            list = genre_scpore.sort_values('popul').head(10)
+            st.dataframe(genre_scpore.loc[:,['popul','sum']].sort_values('popul'))
+            list = genre_scpore.sort_values('popul')
             select = st.selectbox('인기가 많았던 장르별 드라마',list )
             st.dataframe(df_main[df['Genres'].str.contains(str(select),na=False)],)
             
 
         if status == '평점순':    
-            st.subheader('평가가 좋은 작품이 많은 장르입니다')
-            st.dataframe(genre_scpore.loc[:,['score']].sort_values('score',ascending=False))
+            st.subheader('명작이 많은 장르입니다')
+            st.dataframe(genre_scpore.loc[:,['score','sum']].sort_values('score',ascending=False))
             list = genre_scpore.sort_values('score',ascending=False).head(10)
             st.subheader('작품 평가가 좋은 장르별 드라마')
             select = st.selectbox('',list )
@@ -50,7 +62,7 @@ def run_sc_app() :
 
         if status == '화제순':
             st.subheader('화제가된 작품이 많은 장르입니다')
-            st.dataframe(genre_scpore.loc[:,['review']].sort_values('review',ascending=False))
+            st.dataframe(genre_scpore.loc[:,['review','sum']].sort_values('review',ascending=False))
             list = genre_scpore.sort_values('review',ascending=False).head(10)
             st.subheader('화제가 된 장르별 드라마')
             select = st.selectbox('',list )
@@ -60,13 +72,13 @@ def run_sc_app() :
         
     if select_rank =='태그별' :
         tag_slporesum = pd.read_csv('data/tag_slporesum.csv',index_col='tags')
-      
+        status = st.radio('',['인기순','평점순','화제순'])
       
 
         if status == '인기순' :
             st.subheader('인기가 많은 드라마 태그입니다')
-            st.dataframe(tag_slporesum[tag_slporesum['sum'] >10].loc[:,['Popularity']].sort_values('Popularity'))
-            list = tag_slporesum[tag_slporesum['sum'] >10].sort_values('Popularity').head(10)
+            st.dataframe(tag_slporesum[tag_slporesum['sum'] >10].loc[:,['Popularity','sum']].sort_values('Popularity'))
+            list = tag_slporesum[tag_slporesum['sum'] >10].sort_values('Popularity')
             st.subheader('인기 태그별 드라마 목록 입니다 입니다')
             select = st.selectbox('',list )
             st.dataframe(df_main[df['Tags'].str.contains(str(select),na=False)],)
@@ -74,8 +86,8 @@ def run_sc_app() :
 
         if status == '평점순':
             st.subheader('평가가 좋은 작품의 태그 입니다')
-            st.dataframe(tag_slporesum[tag_slporesum['sum'] >10].loc[:,['score']].sort_values('score',ascending=False))
-            list = tag_slporesum[tag_slporesum['sum'] >10].sort_values('score',ascending=False).head(10)
+            st.dataframe(tag_slporesum[tag_slporesum['sum'] >10].loc[:,['score','sum']].sort_values('score',ascending=False))
+            list = tag_slporesum[tag_slporesum['sum'] >10].sort_values('score',ascending=False)
             st.subheader('태그별 평가가 좋은 대표 드라마 목록 입니다')
             select = st.selectbox('',list )
             st.dataframe(df_main[df['Tags'].str.contains(str(select),na=False)],)
@@ -83,8 +95,8 @@ def run_sc_app() :
 
         if status == '화제순':
             st.subheader('화제가 된 작품들의 태그입니다')
-            st.dataframe(tag_slporesum[tag_slporesum['sum'] >10].loc[:,['review']].sort_values('review',ascending=False))
-            list = tag_slporesum[tag_slporesum['sum'] >10].sort_values('review',ascending=False).head(10)
+            st.dataframe(tag_slporesum[tag_slporesum['sum'] >10].loc[:,['review','sum']].sort_values('review',ascending=False))
+            list = tag_slporesum[tag_slporesum['sum'] >10].sort_values('review',ascending=False)
             st.subheader('태그별 드라마 목록 입니다')
             select = st.selectbox('',list )
             st.dataframe(df_main[df['Tags'].str.contains(str(select),na=False)],)
@@ -92,31 +104,35 @@ def run_sc_app() :
 
     if select_rank =='배우별' :
         actor_slporesum = pd.read_csv('data/actor_slporesum.csv',index_col='actors')
+        status = st.radio('',['인기순','평점순','화제순'])
 
         if status == '인기순' :
             st.subheader('인기 작품에 많이 출연한 배우입니다')
-            st.dataframe(actor_slporesum[actor_slporesum['sum'] >5].loc[:,['Popularity']].sort_values('Popularity'))
-            list = actor_slporesum[actor_slporesum['sum'] >10].sort_values('Popularity').head(10)
+            st.dataframe(actor_slporesum[actor_slporesum['sum'] >5].loc[:,['Popularity','sum']].sort_values('Popularity'))
+            list = actor_slporesum[actor_slporesum['sum'] >5].sort_values('Popularity')
             st.subheader('배우 필모그래피 보기')
             select = st.selectbox('',list )
-            st.dataframe(df_main[df['actors'].str.contains(str(select),na=False)],)
+            st.dataframe(df_main[df['actors'].str.contains(str(select),na=False)].sort_values('Popularity'),)
         if status == '평점순':
             st.subheader('명작에 많이 출연한 배우 입니다')
-            st.dataframe(actor_slporesum[actor_slporesum['sum'] >5].loc[:,['socre']].sort_values('socre',ascending=False))
-            list = actor_slporesum[actor_slporesum['sum'] >5].sort_values('socre',ascending=False).head(10)
+            st.dataframe(actor_slporesum[actor_slporesum['sum'] >5].loc[:,['socre','sum']].sort_values('socre',ascending=False))
+            list = actor_slporesum[actor_slporesum['sum'] >5].sort_values('socre',ascending=False)
             st.subheader('배우 필모그래피 보기')
             select = st.selectbox('',list )
-            st.dataframe(df_main[df['actors'].str.contains(str(select),na=False)],)            
+            st.dataframe(df_main[df['actors'].str.contains(str(select),na=False)].sort_values('score',ascending=False),)            
 
 
 
         if status == '화제순':
             st.subheader('화제가 많이되는 작품에 출연한 배우입니다')
-            st.dataframe(actor_slporesum[actor_slporesum['sum'] >5].loc[:,['review']].sort_values('review',ascending=False))
-            list = actor_slporesum[actor_slporesum['sum'] >5].sort_values('review',ascending=False).head(10)
+            st.dataframe(actor_slporesum[actor_slporesum['sum'] >5].loc[:,['review','sum']].sort_values('review',ascending=False))
+            list = actor_slporesum[actor_slporesum['sum'] >5].sort_values('review',ascending=False)
             st.subheader('배우 필모그래피 보기')
             select = st.selectbox('',list )
-            st.dataframe(df_main[df['actors'].str.contains(str(select),na=False)],)
-
+            st.dataframe(df_main[df['actors'].str.contains(str(select),na=False)].sort_values('scored by',ascending=False),)
+    
+    if select_rank =='평점별' :
+        score = st.slider('평점별', 4.0 ,10.0,step=0.5)
+        st.dataframe(df[(df['score'] <= score ) & (df['score'] >= score -0.5 )])
 
 
